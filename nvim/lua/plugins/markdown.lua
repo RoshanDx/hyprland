18a8cf31453e300e42a8d6a643f322f04b040e3f
Roshan <roshandx88@gmail.com>
2025-05-05 22:03:34 +0800

    update neovim, add render-markdown and new wallpaper

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+		opts = {},
+		config = function()
+			require("render-markdown").setup({
+				completions = { lsp = { enabled = true } },
+			})
+
+			require("render-markdown").setup({
+				checkbox = {
+					custom = {
+						important = {
+							raw = "[~]",
+							rendered = "󰓎 ",
+							highlight = "DiagnosticWarn",
+						},
+						todo = { rendered = "◯ " },
+					},
+					unchecked = { icon = "✘ " },
+					checked = { icon = "✔ " },
+				},
+			})
+		end,
+	},
+}
